cad92e145a9e9887d725544a88b2ee0ef69e6787
KangLin <kl222@126.com>
2019-11-13 10:33:58 +0800

    Modify ci

--- ci/build-install-tools-windows.sh
+++ ci/build-install-tools-windows.sh
@@ -20,41 +20,51 @@ fi
 if [ ! -d "${TOOLS_DIR}" ]; then
     mkdir ${TOOLS_DIR}
 fi
-
 cd ${TOOLS_DIR}
 
 # Qt qt安装参见：https://github.com/benlau/qtci  
 if [ -n "${QT_VERSION}" ]; then
-    QT_DIR=C:/projects/SerialPortAssistant/Tools/Qt/${QT_VERSION}
+    QT_DIR=C:/projects/${APPVEYOR_PROJECT_NAME}/Tools/Qt/${QT_VERSION}
     if [ ! -d "${QT_DIR}" ]; then
-        wget -c --no-check-certificate -nv http://download.qt.io/official_releases/qt/${QT_VERSION_DIR}/${QT_VERSION}/qt-opensource-windows-x86-android-${QT_VERSION}.exe
-        bash ${SOURCE_DIR}/ci/qt-installer.sh qt-opensource-windows-x86-android-${QT_VERSION}.exe ${QT_DIR}
-        rm qt-opensource-windows-x86-android-${QT_VERSION}.exe
+        wget -c --no-check-certificate -nv http://download.qt.io/official_releases/qt/${QT_VERSION_DIR}/${QT_VERSION}/qt-opensource-windows-x86-${QT_VERSION}.exe
+        bash ${SOURCE_DIR}/ci/qt-installer.sh qt-opensource-windows-x86-${QT_VERSION}.exe ${QT_DIR}
+        rm qt-opensource-windows-x86-${QT_VERSION}.exe
     fi
 fi
 
+cd ${TOOLS_DIR}
+
 #下载ANT 
-wget -c -nv http://apache.fayea.com//ant/binaries/apache-ant-1.10.1-bin.tar.gz
-tar xzf apache-ant-1.10.1-bin.tar.gz
-rm -f apache-ant-1.10.1-bin.tar.gz
-mv apache-ant-1.10.1 apache-ant
+#wget -c -nv http://apache.fayea.com//ant/binaries/apache-ant-1.10.1-bin.tar.gz
+#tar xzf apache-ant-1.10.1-bin.tar.gz
+#rm -f apache-ant-1.10.1-bin.tar.gz
+#mv apache-ant-1.10.1 apache-ant
+
+cd ${TOOLS_DIR}
 
 #Download android sdk  
 if [ ! -d "${TOOLS_DIR}/android-sdk" ]; then
-    wget -c -nv https://dl.google.com/android/android-sdk_r24.4.1-windows.zip
-    unzip -q android-sdk_r24.4.1-windows.zip
-    mv android-sdk-windows android-sdk
-    rm android-sdk_r24.4.1-windows.zip
-    (sleep 5 ; while true ; do sleep 1 ; printf 'y\r\n' ; done ) \
-    | android-sdk/tools/android.bat update sdk -u -t tool,android-18,android-24,extra,platform,platform-tools,build-tools-24.0.1
-fi
+    cd ${TOOLS_DIR}
 
-#下载android ndk  
-if [ ! -d "${TOOLS_DIR}/android-ndk" ]; then
-    wget -c -nv http://dl.google.com/android/ndk/android-ndk-r10e-windows-x86.exe
-    ./android-ndk-r10e-windows-x86.exe > /dev/null
-    mv android-ndk-r10e android-ndk
-    rm android-ndk-r10e-windows-x86.exe
+    ANDROID_STUDIO_VERSION=191.5900203
+    wget -c -nv https://dl.google.com/dl/android/studio/ide-zips/3.5.1.0/android-studio-ide-${ANDROID_STUDIO_VERSION}-windows.zip
+    unzip -q android-studio-ide-${ANDROID_STUDIO_VERSION}-windows.zip
+    rm android-studio-ide-${ANDROID_STUDIO_VERSION}-windows.zip
+    export JAVA_HOME=${TOOLS_DIR}/android-studio/jre
+    export PATH=${JAVA_HOME}/bin:$PATH
+    
+    ANDROID_SDK_VERSION=4333796
+    wget -c -nv https://dl.google.com/android/repository/sdk-tools-windows-${ANDROID_SDK_VERSION}.zip
+    mkdir android-sdk
+    cd android-sdk
+    mv ../sdk-tools-windows-${ANDROID_SDK_VERSION}.zip .
+    unzip -q sdk-tools-windows-${ANDROID_SDK_VERSION}.zip
+    rm sdk-tools-windows-${ANDROID_SDK_VERSION}.zip
+
+    echo "Install sdk and ndk ......"
+    (sleep 5 ; num=0 ; while [ $num -le 5 ] ; do sleep 1 ; num=$(($num+1)) ; printf 'y\r\n' ; done ) \
+    | ./tools/bin/sdkmanager.bat "platform-tools" "build-tools;28.0.3" "platforms;${ANDROID_API}" "ndk-bundle"
+    cd ${TOOLS_DIR}
 fi
 
 cd ${SOURCE_DIR}
--- ci/build.sh
+++ ci/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -ev
+set -e
 
 SOURCE_DIR=`pwd`
 if [ -n "$1" ]; then
@@ -13,14 +13,36 @@ if [ "$BUILD_TARGERT" = "android" ]; then
     export ANDROID_NDK_ROOT=${SOURCE_DIR}/Tools/android-ndk
     if [ -n "$APPVEYOR" ]; then
         export JAVA_HOME="/C/Program Files (x86)/Java/jdk1.8.0"
+        export ANDROID_NDK_ROOT=${SOURCE_DIR}/Tools/android-sdk/ndk-bundle
     fi
-    export QT_ROOT=${SOURCE_DIR}/Tools/Qt/${QT_VERSION}/${QT_VERSION}/android_armv7
+    if [ "$TRAVIS" = "true" ]; then
+        export JAVA_HOME=${SOURCE_DIR}/Tools/android-studio/jre
+        #export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+    fi
+    case $BUILD_ARCH in
+        arm*)
+            export QT_ROOT=${SOURCE_DIR}/Tools/Qt/${QT_VERSION}/${QT_VERSION}/android_armv7
+            ;;
+        x86)
+        export QT_ROOT=${SOURCE_DIR}/Tools/Qt/${QT_VERSION}/${QT_VERSION}/android_x86
+        ;;
+    esac
     export PATH=${SOURCE_DIR}/Tools/apache-ant/bin:$JAVA_HOME:$PATH
+    export ANDROID_SDK=${ANDROID_SDK_ROOT}
+    export ANDROID_NDK=${ANDROID_NDK_ROOT}
 fi
 
 if [ "${BUILD_TARGERT}" = "unix" ]; then
-    QT_DIR=${SOURCE_DIR}/Tools/Qt/${QT_VERSION}
-    export QT_ROOT=${QT_DIR}/${QT_VERSION}/gcc_64
+    if [ "$DOWNLOAD_QT" = "TRUE" ]; then
+        QT_DIR=${SOURCE_DIR}/Tools/Qt/${QT_VERSION}
+        export QT_ROOT=${QT_DIR}/${QT_VERSION}/gcc_64
+    else
+        #source /opt/qt${QT_VERSION_DIR}/bin/qt${QT_VERSION_DIR}-env.sh
+        export QT_ROOT=/opt/qt${QT_VERSION_DIR}
+    fi
+    export PATH=$QT_ROOT/bin:$PATH
+    export LD_LIBRARY_PATH=$QT_ROOT/lib/i386-linux-gnu:$QT_ROOT/lib:$LD_LIBRARY_PATH
+    export PKG_CONFIG_PATH=$QT_ROOT/lib/pkgconfig:$PKG_CONFIG_PATH
 fi
 
 if [ "$BUILD_TARGERT" != "windows_msvc" ]; then
@@ -29,6 +51,28 @@ if [ "$BUILD_TARGERT" != "windows_msvc" ]; then
         RABBIT_MAKE_JOB_PARA="-j2"
     fi
 fi
+
+if [ "$BUILD_TARGERT" = "windows_mingw" \
+    -a -n "$APPVEYOR" ]; then
+    export PATH=/C/Qt/Tools/mingw${TOOLCHAIN_VERSION}/bin:$PATH
+fi
+TARGET_OS=`uname -s`
+case $TARGET_OS in
+    MINGW* | CYGWIN* | MSYS*)
+        export PKG_CONFIG=/c/msys64/mingw32/bin/pkg-config.exe
+        if [ "$BUILD_TARGERT" = "android" ]; then
+            ANDROID_NDK_HOST=windows-x86_64
+            if [ ! -d $ANDROID_NDK/prebuilt/${ANDROID_NDK_HOST} ]; then
+                ANDROID_NDK_HOST=windows
+            fi
+            CONFIG_PARA="${CONFIG_PARA} -DCMAKE_MAKE_PROGRAM=make" #${ANDROID_NDK}/prebuilt/${ANDROID_NDK_HOST}/bin/make.exe"
+        fi
+        ;;
+    Linux* | Unix*)
+    ;;
+    *)
+    ;;
+esac
 
 export PATH=${QT_ROOT}/bin:$PATH
 echo "PATH:$PATH"
